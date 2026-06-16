d4d5bb2c1d86b303652d24c903ab1dfa6f4ab419
Lucas Brovetto <lucasbrovetto@gmail.com>
2026-06-16 14:54:59 -0300

    feat: configure postgresql datasource

---
+++ services/transaction-service/src/main/java/com/financialintegration/transactionservice/entity/TransactionStatus.java
@@ -0,0 +1,7 @@
+package com.financialintegration.transactionservice.entity;
+
+public enum TransactionStatus {
+    PENDING,
+    APPROVED,
+    DECLINED
+}
---
+++ services/transaction-service/src/main/java/com/financialintegration/transactionservice/entity/TransactionType.java
@@ -0,0 +1,7 @@
+package com.financialintegration.transactionservice.entity;
+
+public enum TransactionType {
+    SALE,
+    REFUND,
+    REVERSAL
+}
